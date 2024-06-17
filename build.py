569f945933f0cbb165ab958123328103547e6909
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-06-17 14:45:38 +0300

    Fix build script

--- build.py
+++ build.py
@@ -25,9 +25,9 @@ lang_content = json.dumps(lang_content, separators=(',',':'), ensure_ascii=False
 
 # Insert CSS and JS into HTML
 combined = html_content.replace(
-    '<link rel="stylesheet" href="app.css">', f'<style>{css_content}</style>'
+    '<link rel="stylesheet" href="./app.css">', f'<style>{css_content}</style>'
 ).replace(
-    '<script src="app.js"></script>', f'<script>{js_content}</script>'
+    '<script src="./app.js"></script>', f'<script>{js_content}</script>'
 ).replace(
     'require("translations.json")', lang_content
 )
