fbd5a4ab7105200922a08087d9ca12c61de1c05d
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-06-22 19:15:23 +0300

    WebRTC experiment

--- src/transports.js
+++ src/transports.js
@@ -419,8 +419,9 @@ class WebSocketREPL extends Transport {
 class WebRTCTransport extends Transport {
     constructor(peerId = null) {
         super();
-        this.peer = new Peer(peerId);
-        this.connection = null;
+        this.peer = new Peer()
+        this.targetPeerId = peerId
+        this.connection = null
     }
 
     async requestAccess() {
@@ -431,24 +432,29 @@ class WebRTCTransport extends Transport {
         this.info = { id: this.peer.id };
     }
 
-    async connect(targetPeerId) {
-        this.connection = this.peer.connect(targetPeerId);
+    connect() {
+        return new Promise((resolve, reject) => {
+            this.connection = this.peer.connect(this.targetPeerId);
 
-        this.connection.on('open', () => {
-            this.listen();
-        });
+            this.connection.on('open', () => {
+                this.connection.on('data', (data) => {
+                    if (this.receiveCallback) {
+                        this.receiveCallback(data)
+                    }
+                })
+                resolve()
+            })
 
-        this.connection.on('close', () => {
-            if (this.disconnectCallback) {
-                this.disconnectCallback();
-            }
-        });
+            this.connection.on('error', (err) => {
+                console.error('WebRTC connection error:', err);
+                reject(err)
+            })
 
-        this.connection.on('error', (err) => {
-            console.error('WebRTC connection error:', err);
-            if (this.disconnectCallback) {
-                this.disconnectCallback();
-            }
+            this.connection.on('close', () => {
+                if (this.disconnectCallback) {
+                    this.disconnectCallback()
+                }
+            })
         });
     }
 
@@ -460,17 +466,11 @@ class WebRTCTransport extends Transport {
     }
 
     async writeBytes(data) {
-        if (this.connection) {
+        if (this.connection && this.connection.open) {
             this.connection.send(data);
+        } else {
+            throw new Error('Connection is not open.');
         }
-    }
-
-    listen() {
-        this.connection.on('data', (data) => {
-            if (this.receiveCallback) {
-                this.receiveCallback(data);
-            }
-        });
     }
 }
 
