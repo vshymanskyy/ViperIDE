e58ea1370c3d1a445852b25869b66c86297e5fbc
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-07-02 23:48:34 +0300

    Update build system

--- build.py
+++ build.py
@@ -1,24 +1,61 @@
 #!/usr/bin/env python3
 
-import os, sys, json, glob, time
+import time
+import json, glob
+from os import remove as rm, system, path, makedirs
+from shutil import copyfile as cp, copytree, rmtree
 
 def readfile(fn):
     with open(fn, 'r', encoding='utf-8') as f:
         return f.read()
 
-# Insert CSS and JS into HTML
-combined = readfile(sys.argv[1]).replace(
-    '<link rel="stylesheet" href="./common.css">', '<style>\n' + readfile('src/app_common.css') + '\n</style>'
-).replace(
-    '<link rel="stylesheet" href="./app.css">', '<style>\n' + readfile('build/app.css') + '\n</style>'
-).replace(
-    '<script src="./app.js"></script>', '<script>\n' + readfile('build/app.js') + '\n</script>'
-).replace(
-    'window.VIPER_IDE_BUILD', str(int(time.time() * 1000))
-)
+def gen_translations(src, dst):
+    result = {}
+    for fn in glob.glob('*.json', root_dir=src):
+        lang = fn.replace('.json', '')
+        result[lang] = json.loads(readfile(src + fn))
+    with open(dst, 'w', encoding='utf-8') as f:
+        json.dump(result, f, separators=(',',':'), ensure_ascii=False, sort_keys=True)
 
-# Write the combined content
-with open(sys.argv[2], 'w', encoding='utf-8') as f:
-    f.write(combined)
+def combine(src, dst):
+    # Insert CSS and JS into HTML
+    combined = readfile(src).replace(
+        '<link rel="stylesheet" href="./common.css">', '<style>\n' + readfile('src/app_common.css') + '\n</style>'
+    ).replace(
+        '<link rel="stylesheet" href="./app.css">', '<style>\n' + readfile('build/app.css') + '\n</style>'
+    ).replace(
+        '<script src="./app.js"></script>', '<script>\n' + readfile('build/app.js') + '\n</script>'
+    ).replace(
+        'window.VIPER_IDE_BUILD', str(int(time.time() * 1000))
+    )
 
-print('Files combined successfully!')
+    # Write the combined content
+    with open(dst, 'w', encoding='utf-8') as f:
+        f.write(combined)
+
+if __name__ == "__main__":
+    rmtree("build", ignore_errors=True)
+    makedirs("build")
+    gen_translations("./lang/", "build/translations.json")
+
+    if not path.isdir("node_modules"):
+        system("npm install")
+    system("npm run build")
+
+    system("jq -c . < manifest.json > ./build/manifest.json")
+    combine("src/ViperIDE.html",   "build/index.html")
+    combine("src/bridge.html",     "build/bridge.html")
+    combine("src/benchmark.html",  "build/benchmark.html")
+
+    copytree("./assets", "./build/assets")
+    cp("node_modules/@pybricks/mpy-cross-v6/build/mpy-cross-v6.wasm", "./build/mpy-cross-v6.wasm")
+    cp("./src/webrepl_content.js", "./build/webrepl_content.js")
+
+    # Cleanup
+    rm("build/app.css")
+    rm("build/app.js")
+    rm("build/translations.json")
+
+    print()
+    print("Build complete.")
+
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,16 +4,6 @@ import json from '@rollup/plugin-json'
 import postcss from 'rollup-plugin-postcss'
 import terser from '@rollup/plugin-terser'
 //import { eslint } from 'rollup-plugin-eslint'
-import { execSync } from 'child_process'
-import path from 'path'
-
-const prepare = () => ({
-  name: 'prepare',
-  buildStart() {
-    execSync('mkdir -p build')
-    execSync('python3 lang/_gen.py')
-  }
-})
 
 export default [{
   input: './src/app.js',
@@ -23,7 +13,6 @@ export default [{
     format: 'iife',
   },
   plugins: [
-    prepare(),
     postcss({
       extract: 'app.css',
       minimize: true,
