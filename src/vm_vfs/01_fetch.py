e4013d7ed652c6d104e5a184dced03823636a8a7
vshymanskyy <vshymanskyi@gmail.com>
2026-08-02 13:07:35 +0300

    Update VM vfs

--- src/vm_vfs/01_fetch.py
+++ src/vm_vfs/01_fetch.py
@@ -6,6 +6,7 @@ async def task():
     print(f"Fetching {url}...")
     res = await js.fetch(url)
     data = await res.json()
+    print()
     print(data.author, ":", data.quote)
 
 asyncio.create_task(task())
